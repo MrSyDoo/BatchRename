a2231c7ccd2f6fcb51610bc963235995849cc750
MrSyDoo <166733395+MrSyDoo@users.noreply.github.com>
2025-05-16 18:15:16 +0530

    Create ffmpeg.py

---
+++ helper/ffmpeg.py
@@ -0,0 +1,105 @@
+import time
+import os
+import asyncio
+from PIL import Image
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+
+
+import os, time, asyncio, subprocess, json
+from helper.utils import metadata_text
+
+async def change_metadata(input_file, output_file, metadata):
+    author, title, video_title, audio_title, subtitle_title = await metadata_text(metadata)
+    
+    # Get the video metadata
+    output = subprocess.check_output(['ffprobe', '-v', 'error', '-show_streams', '-print_format', 'json', input_file])
+    data = json.loads(output)
+    streams = data['streams']
+
+    # Create the FFmpeg command to change metadata
+    cmd = [
+        'ffmpeg',
+        '-i', input_file,
+        '-map', '0',  # Map all streams
+        '-c:v', 'copy',  # Copy video stream
+        '-c:a', 'copy',  # Copy audio stream
+        '-c:s', 'copy',  # Copy subtitles stream
+        '-metadata', f'title={title}',
+        '-metadata', f'author={author}',
+    ]
+
+    # Add title to video stream
+    for stream in streams:
+        if stream['codec_type'] == 'video' and video_title:
+            cmd.extend([f'-metadata:s:{stream["index"]}', f'title={video_title}'])
+        elif stream['codec_type'] == 'audio' and audio_title:
+            cmd.extend([f'-metadata:s:{stream["index"]}', f'title={audio_title}'])
+        elif stream['codec_type'] == 'subtitle' and subtitle_title:
+            cmd.extend([f'-metadata:s:{stream["index"]}', f'title={subtitle_title}'])
+
+    cmd.extend(['-metadata', f'comment=Added by @Digital_Rename_Bot'])
+    cmd.extend(['-f', 'matroska']) # support all format 
+    cmd.append(output_file)
+    print(cmd)
+    
+    # Execute the command
+    try:
+        subprocess.run(cmd, check=True)
+        return True
+    except subprocess.CalledProcessError as e:
+        print("FFmpeg Error:", e.stderr)
+        return False
+        
+async def fix_thumb(thumb):
+    width = 0
+    height = 0
+    try:
+        if thumb != None:
+            parser = createParser(thumb)
+            metadata = extractMetadata(parser)
+            if metadata.has("width"):
+                width = metadata.get("width")
+            if metadata.has("height"):
+                height = metadata.get("height")
+                
+            # Open the image file
+            with Image.open(thumb) as img:
+                # Convert the image to RGB format and save it back to the same file
+                img.convert("RGB").save(thumb)
+            
+                # Resize the image
+                resized_img = img.resize((width, height))
+                
+                # Save the resized image in JPEG format
+                resized_img.save(thumb, "JPEG")
+            parser.close()
+    except Exception as e:
+        print(e)
+        thumb = None 
+       
+    return width, height, thumb
+    
+async def take_screen_shot(video_file, output_directory, ttl):
+    out_put_file_name = f"{output_directory}/{time.time()}.jpg"
+    file_genertor_command = [
+        "ffmpeg",
+        "-ss",
+        str(ttl),
+        "-i",
+        video_file,
+        "-vframes",
+        "1",
+        out_put_file_name
+    ]
+    process = await asyncio.create_subprocess_exec(
+        *file_genertor_command,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+    stdout, stderr = await process.communicate()
+    e_response = stderr.decode().strip()
+    t_response = stdout.decode().strip()
+    if os.path.lexists(out_put_file_name):
+        return out_put_file_name
+    return None
