49ead5cb49e83bcc1604c20c1d4933a33f4099b3
MrSyDoo <166733395+MrSyDoo@users.noreply.github.com>
2025-05-16 18:05:52 +0530

    Create start.py

---
+++ plugins/start.py
@@ -0,0 +1,38 @@
+import random
+import logging
+from pyrogram import Client, filters, enums
+from pyrogram.enums import ParseMode
+from pyrogram.errors import FloodWait, ChatAdminRequired
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto, CallbackQuery
+from helper.database import db
+from config import Config, Txt
+from info import AUTH_CHANNEL
+from helper.utils import is_req_subscribed
+import humanize
+from time import sleep
+
+logger = logging.getLogger(__name__)
+
+@Client.on_message(filters.private & filters.command("start"))
+async def start(client, message):
+
+    if message.from_user.id in Config.BANNED_USERS:
+        await message.reply_text("Sorry, You are banned.")
+        return
+
+    user = message.from_user
+    await db.add_user(client, message)
+    button = InlineKeyboardMarkup([[
+        InlineKeyboardButton(
+            '⛅ Uᴘᴅᴀᴛᴇꜱ', url=f'https://t.me/{Config.UPDATES}'),
+        InlineKeyboardButton(
+            ' Sᴜᴘᴘᴏʀᴛ 🌨️', url=f'https://t.me/{Config.SUPPORT}')
+    ], [
+        InlineKeyboardButton('❄️ Δʙᴏᴜᴛ', callback_data='about'),
+        InlineKeyboardButton(' Hᴇʟᴩ ❗', callback_data='help')
+    ], [InlineKeyboardButton('⚙️ sᴛΔᴛs ⚙️', callback_data='stats')]])
+    if Config.PICS:
+        await message.reply_photo(random.choice(Config.PICS), caption=Txt.START_TXT.format(user.mention), reply_markup=button)
+    else:
+        await message.reply_text(text=Txt.START_TXT.format(user.mention), reply_markup=button, disable_web_page_preview=True)
+        
