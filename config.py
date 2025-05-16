eb63f0ad3bd4bc0411347a3329e2da347133bd5d
MrSyDoo <166733395+MrSyDoo@users.noreply.github.com>
2025-05-16 17:59:27 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,112 @@
+import re
+import os
+import time
+
+id_pattern = re.compile(r'^.\d+$')
+
+
+class Config(object):
+    # pyro client config
+    API_ID = os.environ.get("API_ID", "")  # ⚠️ Required
+    API_HASH = os.environ.get("API_HASH", "")  # ⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")  # ⚠️ Required
+
+    # database config
+    DB_NAME = os.environ.get("DB_NAME", "cluster0")
+    DB_URL = os.environ.get("DB_URL", "")  # ⚠️ Required
+
+    SUPPORT = os.environ.get("SUPPORT", "syd_xyz") #Without @ Or Modify
+    UPDATES = os.environ.get("UPDATES", "syd_xyz") #Without @
+
+    # other configs
+    BOT_UPTIME = time.time()
+    PICS = os.environ.get("PICS", 'https://graph.org/file/8c8372dfa0e0ddf8da91d.jpg https://graph.org/file/3b2b8110f6f57f7fc5c74.jpg  https://graph.org/file/1bd6fa19297caf4189c61.jpg  ').split()
+    ADMIN = [int(admin) if id_pattern.search(
+        admin) else admin for admin in os.environ.get('ADMIN', '').split()]  # ⚠️ Required
+
+    FORCE_SUB = os.environ.get("FORCE_SUB", "") # ⚠️ Required Username without @
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))  # ⚠️ Required
+    FLOOD = int(os.environ.get("FLOOD", '10'))
+    BANNED_USERS = set(int(x) for x in os.environ.get(
+        "BANNED_USERS", "1234567890").split())
+
+    # wes response configuration
+    WEBHOOK = bool(os.environ.get("WEBHOOK", True))
+    PORT = int(os.environ.get("PORT", "8080"))
+
+
+class Txt(object):
+    # part of text configuration
+    START_TXT = """<b>Hᴀɪ {} 👋,
+I Δᴍ Aɴ Aᴅᴠᴀɴᴄᴇᴅ Aɴᴅ Yᴇᴛ Pᴏᴡᴇʀꜰᴜʟ Rᴇɴᴀᴍᴇ Bᴏᴛ Yᴏᴜ Hᴀᴅ єᴠєʀ Sᴇᴇɴ ᴩʀᴏʙᴀʙʟʏ.... 🪄
+Yᴏᴜ Cᴀɴ Rᴇɴᴀᴍᴇ & Cʜᴀɴɢᴇ Tʜᴜᴍʙɴᴀɪʟ Oꜰ Δɴʏ Fɪʟᴇ Fᴏʀ <u>FR̊ΞΞ</u>"""
+
+    ABOUT_TXT = """<b>╭───────────⍟
+➥ ᴍy ɴᴀᴍᴇ : {}
+➥ Oᴡɴᴇʀ : <a href=https://t.me/vizean>Vɪᴢᴇᴀɴ</a> 
+➥ Lᴀɴɢᴜᴀɢᴇ : ᴩʏᴛʜᴏɴ3
+➥ Dᴀᴛᴀ Bᴀꜱᴇ: ᴍᴏɴɢᴏᴅʙ
+➥ ᴍʏ ꜱᴇʀᴠᴇʀ : ᴩʀɪᴠᴀᴛᴇ
+➥ Vᴇʀsɪᴏɴ : v1.0
+╰───────────────⍟ """
+
+    HELP_TXT = """
+◽ <b><u>Hᴏᴡ Tᴏ Sᴇᴛ Tʜᴜᴍʙɴɪʟᴇ</u></b>
+  
+<b>•></b> /start Tʜᴇ Bᴏᴛ Aɴᴅ Sᴇɴᴅ Aɴy Pʜᴏᴛᴏ Tᴏ Aᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟy Sᴇᴛ Tʜᴜᴍʙɴɪʟᴇ.
+<b>•></b> /del_thumb Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Dᴇʟᴇᴛᴇ Yᴏᴜʀ Oʟᴅ Tʜᴜᴍʙɴɪʟᴇ.
+<b>•></b> /view_thumb Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Vɪᴇᴡ Yᴏᴜʀ Cᴜʀʀᴇɴᴛ Tʜᴜᴍʙɴɪʟᴇ.
+
+
+<u><b><blockqoute>Jᴜꜱᴛ ꜱᴇɴᴅ ᴛʜᴇ ᴩɪᴄᴛᴜʀᴇ.. ⚡</blockqoute></u></b>
+
+◽ <b><u>Hᴏᴡ Tᴏ Sᴇᴛ Cᴜꜱᴛᴏᴍ Cᴀᴩᴛɪᴏɴ</u></b>
+
+<b>•></b> /set_caption - Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Sᴇᴛ ᴀ Cᴜꜱᴛᴏᴍ Cᴀᴩᴛɪᴏɴ
+<b>•></b> /see_caption - Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Vɪᴇᴡ Yᴏᴜʀ Cᴜꜱᴛᴏᴍ Cᴀᴩᴛɪᴏɴ
+<b>•></b> /del_caption - Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Dᴇʟᴇᴛᴇ Yᴏᴜʀ Cᴜꜱᴛᴏᴍ Cᴀᴩᴛɪᴏɴ
+Exᴀᴍᴩʟᴇ:- <code> /set_caption 📕 Fɪʟᴇ Nᴀᴍᴇ: {filename}
+💾 Sɪᴢᴇ: {filesize}
+⏰ Dᴜʀᴀᴛɪᴏɴ: {duration} </code>
+
+◽ <b><u>Hᴏᴡ Tᴏ Rᴇɴᴀᴍᴇ A Fɪʟᴇ</u></b>
+<b>•></b> Sᴇɴᴅ Aɴy Fɪʟᴇ Aɴᴅ Tyᴩᴇ Nᴇᴡ Fɪʟᴇ Nᴀᴍᴇ \nAɴᴅ Sᴇʟᴇᴄᴛ Tʜᴇ Fᴏʀᴍᴀᴛ [ document, video, audio ].           
+
+◽ <b><u>Sᴇᴛ ꜱᴜꜰꜰɪx ᴀɴᴅ ᴩʀᴇꜰɪx.</b></u>
+<b>•></b> /set_prefix - Sᴇᴛ ᴩʀᴇꜰɪx(ꜰɪʀꜱᴛ ᴡᴏʀᴅ)
+<b>•></b> /set_suffix - Sᴇᴛ ꜱᴜꜰꜰɪx(ʟᴀꜱᴛ ᴡᴏʀᴅ)
+<b>•></b> /see_prefix - Sᴇᴇ ᴩʀᴇꜰɪx
+<b>•></b> /see_suffix - Sᴇᴇ ꜱᴜꜰꜰɪx
+<b>•></b> /del_prefix - Dᴇʟᴇᴛᴇ ᴩʀᴇꜰɪx
+<b>•></b> /del_suffix - Dᴇʟᴇᴛᴇ ꜱᴜꜰꜰɪx
+"""
+
+    SEND_METADATA = """
+❪ SET CUSTOM METADATA ❫
+
+☞ Fᴏʀ Exᴀᴍᴘʟᴇ:-
+
+◦ <code>--change-title Powered By:- @Kdramaland --change-author @Snowball_Official --change-video-title By:- @Snowball_Official --change-audio-title By :- @Kdramaland --change-subtitle-title Subtitled By :- @Kdramaland</code>
+
+"""
+
+    STATS_TXT = """
+╔════❰ Sᴇʀᴠᴇʀ sᴛᴀᴛS  ❱═❍⊱❁۪۪
+║╭━━━━━━━━━━━━━━━➣
+║┣⪼ ᴜᴩᴛɪᴍᴇ: `{0}`
+║┣⪼ ᴛᴏᴛᴀʟ sᴘᴀᴄᴇ: `{1}`
+║┣⪼ ᴜsᴇᴅ: `{2} ({3}%)`
+║┣⪼ ꜰʀᴇᴇ: `{4}`
+║┣⪼ ᴄᴘᴜ: `{5}%`
+║┣⪼ ʀᴀᴍ: `{6}%`
+║╰━━━━━━━━━━━━━━━➣
+╚══════════════════❍⊱❁۪۪        
+"""
+
+    PROGRESS_BAR = """<b>\n
+╭━━━━❰Pʀᴏɢʀεss ʙΔʀ❱━➣
+┣⪼ 🗃️ Sɪᴢᴇ: {1} | {2}
+┣⪼ ⏳️ Dᴏɴᴇ : {0}%
+┣⪼ 🚀 Sᴩᴇᴇᴅ: {3}/s
+┣⪼ ⏰️ Eᴛᴀ: {4}
+╰━━━━━━━━━━━━━━━➣ </b>"""
